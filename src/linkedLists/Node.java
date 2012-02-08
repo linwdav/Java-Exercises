f14af3c01e9c35c9bc0f629bb609b3f4cead9166
David Lin <linwdav@gmail.com>
2012-02-08 13:40:09 -1000

    Start on linked list exercises

---
+++ src/linkedLists/Node.java
@@ -0,0 +1,86 @@
+package linkedLists;
+
+/**
+ * Node that can be used in linked list.
+ * @author David Lin
+ */
+public class Node {
+
+  private String data;
+  private Node next;
+
+  /**
+   * Constructor.
+   * @param data Data in node
+   */
+  public Node(String data) {
+    this.data = "";
+    next = null;
+  }
+
+  /**
+   * Insert node to end of linked list.
+   * @param endNode Node to be added
+   */
+  public void insert(Node endNode) {
+    Node node = this;
+    while (node.next != null) {
+      node = node.next;
+    }
+    node.next = endNode;
+  }
+
+  /**
+   * Delete node from linked list.
+   * @param head Head of linked list
+   * @param data Delete node with matching data
+   * @return Node Head of linked list
+   */
+  public Node delete(Node head, String data) {
+    Node node = head;
+    if (node.data.compareTo(data) == 0) {
+      return head.next;
+    }
+    while (node.next != null) {
+      if (node.next.data.compareTo(data) == 0) {
+        node.next = node.next.next;
+        return head;
+      }
+      node = node.next;
+    }
+    return null;
+  }
+
+  /**
+   * Get the node's data.
+   * @return Node's data
+   */
+  public String getData() {
+    return data;
+  }
+
+  /**
+   * Set the node's data.
+   * @param data The data to set
+   */
+  public void setData(String data) {
+    this.data = data;
+  }
+
+  /**
+   * Get the next node.
+   * @return Next node.
+   */
+  public Node getNext() {
+    return next;
+  }
+
+  /**
+   * Set the next node.
+   * @param next The next node
+   */
+  public void setNext(Node next) {
+    this.next = next;
+  }
+
+}
---
+++ src/linkedLists/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package linkedLists;
+
+/**
+ * Write code to remove duplicates from an unsorted linked list.
+ * @author David Lin
+ */
+public class RemoveDuplicates {
+
+  /** Constructor. */
+  public RemoveDuplicates() {
+    // Empty
+  }
+
+  /**
+   * Remove duplicate nodes from a linked list.
+   * @param head Head of linked list
+   */
+  public void removeDuplicates(Node head) {
+    Node node = head;
+  }
+
+}
