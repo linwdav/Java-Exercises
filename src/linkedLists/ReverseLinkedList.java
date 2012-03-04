1fe24b1da8433059167e338d63e2e19b361cb6ce
David Lin <linwdav@gmail.com>
2012-03-03 20:10:09 -1000

    Add two ways to reverse a linked list, one by using a stack, and another by using two pointers

--- src/linkedLists/ReverseLinkedList.java
+++ src/linkedLists/ReverseLinkedList.java
@@ -18,22 +18,22 @@ public class ReverseLinkedList {
    * @param head of linked list
    * @return head of reversed linked list
    */
-  public Node reverse(Node head) {
-    
-    Stack<String> s = new Stack<String>();
-    Node temp = head;
-    
+  public Node reverseByStack(Node head) {
+
     // Empty linked list
     if (head == null) {
       return null;
     }
-    
+
+    Stack<String> s = new Stack<String>();
+    Node temp = head;
+
     // Push all item data into a stack in reverse order
     while (temp != null) {
       s.push(temp.getData());
       temp = temp.getNext();
     }
-    
+
     // Reverse by popping from stack
     Node node = new Node(s.pop());
     temp = node;
@@ -41,8 +41,51 @@ public class ReverseLinkedList {
       temp.setNext(new Node(s.pop()));
       temp = temp.getNext();
     }
-    
+
     return node;
+  }
+
+  /**
+   * Reverse a linked list using two pointers.
+   * @param head of linked list
+   * @return head of reversed linked list
+   */
+  public Node reverseByPointers(Node head) {
+
+    // Empty linked list
+    if (head == null) {
+      return null;
+    }
+
+    // Variables
+    int highcount = 1;
+    int lowcount = 0;
+    Node low = head;
+    Node high = head;
+
+    // Get length of linked list by traversing it and set high to last Node
+    while (high.getNext() != null) {
+      high = high.getNext();
+      highcount++;
+    }
+
+    while (lowcount < highcount) {
+      high = head;
+      // Move high pointer to the correct Node
+      for (int i = 1; i < highcount; i++) {
+        high = high.getNext();
+      }
+      // Reverse by data
+      String temp = low.getData();
+      low.setData(high.getData());
+      high.setData(temp);
+      // Increment low and decrement high
+      low = low.getNext();
+      lowcount++;
+      highcount--;
+    }
+
+    return head;
   }
 
 }
--- src/linkedLists/TestReverseLinkedList.java
+++ src/linkedLists/TestReverseLinkedList.java
@@ -11,13 +11,13 @@ import org.junit.Test;
 public class TestReverseLinkedList {
 
   /**
-   * JUnit test on ReverseLinkedList.
+   * JUnit test on ReverseLinkedList::ReverseByStack.
    */
   @Test
-  public void testReverseLinkedList() {
-    
+  public void testReverseByStack() {
+
     ReverseLinkedList test = new ReverseLinkedList();
-    
+
     // Create a linked list
     Node head = new Node("0");
     Node next = head;
@@ -25,26 +25,63 @@ public class TestReverseLinkedList {
       next.setNext(new Node(String.valueOf(i)));
       next = next.getNext();
     }
-    
-    head = test.reverse(head);
-    
+
+    head = test.reverseByStack(head);
+
     // Check for correct reversal from 0123456789 to 9876543210
     next = head;
     for (int i = 9; i > -1; i--) {
       assertEquals("Linked list reversal failed", String.valueOf(i), next.getData());
       next = next.getNext();
     }
-    
+
     // Check for null return value in empty linked list
     next = null;
-    next = test.reverse(next);
+    next = test.reverseByStack(next);
     assertNull("Empty linked list should return null upon reversal", next);
-    
+
     // Check for case where the only node is the head
     next = new Node("hi");
-    next = test.reverse(next);
+    next = test.reverseByStack(next);
     assertNull("Linked list reversal failed for 1 node", next.getNext());
-    
+
+  }
+
+  /**
+   * JUnit test on ReverseLinkedList::ReverseByPointers.
+   */
+  @Test
+  public void testReverseByPointers() {
+
+    ReverseLinkedList test = new ReverseLinkedList();
+
+    // Create a linked list
+    Node head = new Node("0");
+    Node next = head;
+    for (int i = 1; i < 10; i++) {
+      next.setNext(new Node(String.valueOf(i)));
+      next = next.getNext();
+    }
+
+    head = test.reverseByPointers(head);
+
+    // Check for correct reversal from 0123456789 to 9876543210
+    next = head;
+    for (int i = 9; i > -1; i--) {
+      assertEquals("Linked list reversal failed", String.valueOf(i), next.getData());
+      next = next.getNext();
+    }
+
+    // Check for null return value in empty linked list
+    next = null;
+    next = test.reverseByPointers(next);
+    assertNull("Empty linked list should return null upon reversal", next);
+
+    // Check for case where the only node is the head
+    next = new Node("hi");
+    next = test.reverseByPointers(next);
+    assertNull("Linked list reversal failed for 1 node", next.getNext());
+
   }
 
 }
